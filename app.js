ecb2b21b0310767024d99862f8f9cd475144ef41
João Marcos <john2022costa20@gmail.com>
2025-08-15 11:17:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
+let amigos = [];
 
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+
+    if (amigo == '') {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    amigos.push(amigo);
+
+    document.getElementById('amigo').value = '';
+}
+
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML = listaAmigos.innerHTML + '<li>' + amigos[i] + '</li>';
+    }
+}
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+
+    if (amigo == '') {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    amigos.push(amigo);
+
+    document.getElementById('amigo').value = '';
+    
+    atualizarListaAmigos();
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert("Adicione pelo menos um amigo para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O amigo secreto sorteado foi: ${amigoSorteado}`;
+}
